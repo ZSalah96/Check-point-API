a80a57a310d2f528276ed6091ca15284ed33f533
ZSalah96 <154322724+ZSalah96@users.noreply.github.com>
2024-03-27 16:39:15 +0000

    Add files via upload

---
+++ COMPONENT/UserList.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const UserList = () => {
+  const [listOfUsers, setListOfUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        setListOfUsers(response.data);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1>List of Users</h1>
+      <ul>
+        {listOfUsers.map(user => (
+          <li key={user.id}>
+            <strong>Name:</strong> {user.name}, <strong>Email:</strong> {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
